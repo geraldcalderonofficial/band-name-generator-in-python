6ee8eab5b17eb4c2804f9002776ef4ab7f4ff74f
Gerald O. Calderon <geraldcalderonofficial@gmail.com>
2025-05-31 17:46:06 +0800

    update py file

--- band_name_generator.py
+++ band_name_generator.py
@@ -3,23 +3,17 @@
 print("Welcome to the Band Generator")
 
 city = ""
+pet_name = ""
 
-
-
+while True:
     print("What's the name of the city you grew up in?")
     city = input("> ")
 
-    if city == "":
-        print("You haven't entered anything. Please try again.")
-    else:
-        break
 
-
+while True:
     print("What's your pet's name?")
     pet_name = input("> ")
-    if pet_name == "":
-        print("You haven't entered anything. Please try again.")
-    else:
-        break
+    
 
+print()
 
