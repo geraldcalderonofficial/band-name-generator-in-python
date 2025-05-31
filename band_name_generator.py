04a27f15a65ff42783d22ac142c85d6373a1fa4b
Gerald O. Calderon <geraldcalderonofficial@gmail.com>
2025-05-31 17:40:51 +0800

    Update files

---
+++ band_name_generator.py
@@ -0,0 +1,25 @@
+#Band Name Generator
+
+print("Welcome to the Band Generator")
+
+city = ""
+
+
+
+    print("What's the name of the city you grew up in?")
+    city = input("> ")
+
+    if city == "":
+        print("You haven't entered anything. Please try again.")
+    else:
+        break
+
+
+    print("What's your pet's name?")
+    pet_name = input("> ")
+    if pet_name == "":
+        print("You haven't entered anything. Please try again.")
+    else:
+        break
+
+print(f"Your band name could be {city} {pet_name}")
